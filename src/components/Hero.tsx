03761ec69df9cbccb98ac6eba6dde35fe0034e5f
Yoga Bayu Savira <mobom09@gmail.com>
2025-05-03 03:08:32 +0700

    hero image

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -13,7 +13,7 @@ const Hero = () => {
       <div 
         className="absolute inset-0 z-0"
         style={{
-          backgroundImage: "url('https://unsplash.com/photos/an-open-laptop-computer-sitting-on-top-of-a-table-NB2sjtToqmA')",
+          backgroundImage: "url('https://images.unsplash.com/photo-1679501072696-9e7c538d71f9?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
           backgroundSize: "cover",
           backgroundPosition: "center",
           opacity: 1,
